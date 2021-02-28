f52151c7bc99f037bb7dc19465941dd1b922f28b
Meliza Caguimbal <melizacaguimbal34@gmail.com>
2021-02-28 23:46:40 +0800

    add simple sample graphQL API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.png";
 import "./App.css";
+import { API } from "aws-amplify";
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { listSongs } from "./graphql/queries";
+import {
+  createSong as createSongMutation,
+  deleteSong as deleteSongMutation,
+} from "./graphql/mutations";
+
+const initialFormState = { name: "", composer: "", album: "", level: "" };
 
 function App() {
+  const [songs, setSongs] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchSongs();
+  }, []);
+
+  async function fetchSongs() {
+    const apiData = await API.graphql({ query: listSongs });
+    setSongs(apiData.data.listSongs.items);
+  }
+
+  async function createSong() {
+    if (!formData.name || !formData.composer || !formData.album || !formData.level) return;
+    await API.graphql({
+      query: createSongMutation,
+      variables: { input: formData },
+    });
+    setSongs([...songs, formData]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteSong({ id }) {
+    const newNotesArray = songs.filter((note) => note.id !== id);
+    setSongs(newNotesArray);
+    await API.graphql({
+      query: deleteSongMutation,
+      variables: { input: { id } },
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +58,43 @@ function App() {
           >
             Beat Saber
           </a>
+        </div>
+        <h1>Beat Saber Songs</h1>
+        <input
+          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+          placeholder="Name"
+          value={formData.name}
+        />
+        <input
+          onChange={(e) =>
+            setFormData({ ...formData, composer: e.target.value })
+          }
+          placeholder="Composer"
+          value={formData.composer}
+        />
+        <input
+          onChange={(e) => setFormData({ ...formData, album: e.target.value })}
+          placeholder="Album"
+          value={formData.album}
+        />
+        <input
+          onChange={(e) =>
+            setFormData({ ...formData, level: e.target.value })
+          }
+          placeholder="Level"
+          value={formData.level}
+        />
+        <button onClick={createSong}>Add Song</button>
+        <div style={{ marginBottom: 30 }}>
+          {songs.map((song) => (
+            <div key={song.id || song.name}>
+              <h2>{song.name}</h2>
+              <p>{song.composer}</p>
+              <p>{song.album}</p>
+              <p>{song.level}</p>
+              <button onClick={() => deleteSong(song)}>Delete song</button>
+            </div>
+          ))}
         </div>
         <AmplifySignOut />
       </header>
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateSong = /* GraphQL */ `
+  subscription OnCreateSong {
+    onCreateSong {
+      id
+      name
+      composer
+      album
+      level
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateSong = /* GraphQL */ `
+  subscription OnUpdateSong {
+    onUpdateSong {
+      id
+      name
+      composer
+      album
+      level
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteSong = /* GraphQL */ `
+  subscription OnDeleteSong {
+    onDeleteSong {
+      id
+      name
+      composer
+      album
+      level
+      createdAt
+      updatedAt
+    }
+  }
+`;
